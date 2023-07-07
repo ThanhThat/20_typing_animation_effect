2d538ccde44c952d838f848169c879d25d3ce0d1
ThanhThat <thachthanhthat1995@gmail.com>
2023-07-07 19:20:46 +0700

    Done

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+const textAnimate = document.querySelector(".text-animation");
+let index = 0;
+const str = "Thành Thật - Learning Developer";
+
+function typingStr() {
+  if (index <= str.length) {
+    let duration = 100;
+
+    textAnimate.innerHTML += str.charAt(index);
+    index++;
+
+    if (index === str.length) {
+      duration = 3000;
+    }
+
+    if (index > str.length) {
+      index = 0;
+      textAnimate.innerHTML = "";
+    }
+
+    setTimeout(() => {
+      typingStr();
+    }, duration);
+  }
+}
+
+typingStr();
